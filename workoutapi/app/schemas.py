f31aa1a8af1d41575beb14b85087011563eed368
Luanziinn <130407173+Luanziinn@users.noreply.github.com>
2024-07-15 21:07:29 -0300

    Create schemas.py

---
+++ workoutapi/app/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+
+class AtletaBase(BaseModel):
+    nome: str
+    cpf: str
+    centro_treinamento: str
+    categoria: str
+
+class AtletaCreate(AtletaBase):
+    pass
+
+class Atleta(AtletaBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class AtletaResponse(BaseModel):
+    nome: str
+    centro_treinamento: str
+    categoria: str
+
+    class Config:
+        orm_mode = True
